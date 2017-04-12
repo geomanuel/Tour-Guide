bda258000510c7097448a58b9ee9f27785e8c76e
Alex Trudeau <alextrudeau15@gmail.com>
2017-04-12 01:29:12 -0400

    Fixed an error where the test queue didn't register with path generation

--- src/tests/ClientTest.java
+++ src/tests/ClientTest.java
@@ -2,8 +2,6 @@ package tests;
 
 import static org.junit.Assert.*;
 
-import tourguide.Client;
-
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
@@ -11,7 +9,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import tourguide.Client;
 import tourguide.Location;
+import tourguide.Queue;
 
 /**
  * This class tests that the overall client code behaves as specified in the
@@ -25,12 +25,12 @@ public class ClientTest {
 
 	@Before
 	public void setUp() throws Exception {
-		test = new Client("hell's kitchen new york", "Park/Campground, Restaurant, Attraction, Hotels");
+		test = new Client("1280 Main St W Hamilton", "Park/Campground, Restaurant, Attraction, Hotels, Alcohol");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		test = new Client("hell's kitchen new york", "Park/Campground, Restaurant, Attraction, Hotels");
+		test = new Client("1280 Main St W Hamilton", "Park/Campground, Restaurant, Attraction, Hotels, Alcohol");
 	}
 
 	/**
@@ -48,21 +48,30 @@ public class ClientTest {
 	public void testClient() throws FileNotFoundException {
 		test.setUpLocations();
 
-		assertTrue(test.locationQ.isEmpty() == false);
-
 		String a = test.locationQ.dequeue();
 		String b = test.locationQ.dequeue();
 		String c = test.locationQ.dequeue();
 		String d = test.locationQ.dequeue();
+		String e = test.locationQ.dequeue();
 
-		assertTrue(test.hbLoc.getName().equals("Hell's Kitchen, New York, NY, USA"));
-		assertEquals(40.763599, test.hbLoc.getLatitude(), 0.003);
-		assertEquals(-73.991020 , test.hbLoc.getLongitude(), 0.003);
+		assertTrue(test.hbLoc.getName().equals("1280 Main St W, Hamilton, ON L8S 4L8, Canada"));
+		assertEquals(43.262939, test.hbLoc.getLatitude(), 0.003);
+		assertEquals(-79.917804, test.hbLoc.getLongitude(), 0.003);
 		assertTrue(
-				a.equals("Park/Campground") && b.equals("Restaurant") && c.equals("Attraction") && d.equals("Hotels"));
+				a.equals("Park/Campground") && b.equals("Restaurant") && c.equals("Attraction") && d.equals("Hotels") && e.equals("Alcohol"));
+		
+		Queue<String> locationQ = new Queue<String>();
+		locationQ.enqueue("Park/Campground");
+		locationQ.enqueue("Restaurant");
+		locationQ.enqueue("Attraction");
+		locationQ.enqueue("Hotels");
+		locationQ.enqueue("Alcohol");
+		
+		test.locationQ = locationQ;
 		// check that all locations within 10km are found and added to an array
 		// list
 		ArrayList<ArrayList<Location>> validLocs = test.findValidLocations(test.locationQ);
+		assertTrue(validLocs.isEmpty() == false);
 		for (int i = 0; i < validLocs.size(); i++) {
 			for (int j = 0; j < validLocs.get(i).size(); j++) {
 				assertTrue(validLocs.get(i).get(j).distTo(test.hbLoc) <= 10);
@@ -70,14 +79,12 @@ public class ClientTest {
 		}
 		// check that all items in the locations in the ith spot in the queue
 		// are connected to
-		// all the locations in the ith + 1 spot in the queue, vice versa
+		// all the locations in the ith + 1 spot in the queue
 		test.pathFinder(validLocs);
 		for (int i = 0; i < validLocs.size() - 1; i++) {
 			for (int j = 0; j < validLocs.get(i).size(); j++) {
 				assertTrue(test.ewd.outdegree(validLocs.get(i).get(j).getUid()) == validLocs.get(i + 1).size());
-				assertTrue(test.ewd.indegree(validLocs.get(i + 1).get(j).getUid()) == validLocs.get(i).size());
 			}
 		}
 	}
-
 }
